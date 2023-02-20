0498233f94c149889c8e49d3e24cc3d61b0316b1
nfallis96 <nfallis2019@fau.edu>
2023-02-20 14:31:15 -0500

    took html routes out of server

--- server.js
+++ server.js
@@ -9,16 +9,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// HTML routes 
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-// API routes
 app.use(apiRoutes)
 
 // listen on port 3001
